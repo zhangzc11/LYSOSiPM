b3866d2b44f7660c111f1c0a6c55be6b56901fba
zhicai <zhicai@imaczhicai.dyndns.cern.ch>
2018-05-24 13:20:26 +0200

    add BaF2 crystal

--- src/LYSOSiPMDetectorConstruction.cc
+++ src/LYSOSiPMDetectorConstruction.cc
@@ -83,7 +83,7 @@ void LYSOSiPMDetectorConstruction::DefineMaterials() {
 	prelude->AddElement(nist->FindOrBuildElement("Si"),7*perCent);
 	prelude->AddElement(nist->FindOrBuildElement("O"), 18*perCent);
 	prelude->AddElement(nist->FindOrBuildElement("Y"), 4*perCent);
-	G4Material* scintillator = new G4Material("scintillator", prelude_density ,ncomponents=2);
+	G4Material* scintillator = new G4Material("scintillator_LYSO", prelude_density ,ncomponents=2);
 	scintillator->AddMaterial(prelude,99.81*perCent);
 	scintillator->AddElement(nist->FindOrBuildElement("Ce"), 0.19*perCent);
 	G4MaterialPropertiesTable *mpt = new G4MaterialPropertiesTable();
@@ -116,7 +116,6 @@ void LYSOSiPMDetectorConstruction::DefineMaterials() {
 	//mpt->AddConstProperty("SCINTILLATIONYIELD",4/keV);//mimic SiPM pde
 	mpt->AddConstProperty("RESOLUTIONSCALE", 1);
 	mpt->AddConstProperty("FASTTIMECONSTANT",41.0*ns);
-	scintillator->SetMaterialPropertiesTable(mpt);
 
 	//use Marco's absorption length: https://github.com/marco-toli/TB_timing/blob/3f99f77d68aa1c1d395b388e4e509dac17de7211/src/MyMaterials.cc#L2041
 	const G4int nEntries_ABS = 85;
@@ -146,6 +145,62 @@ void LYSOSiPMDetectorConstruction::DefineMaterials() {
 	};
 	mpt->AddProperty("ABSLENGTH", PhotonEnergy_ABS, Absorption, nEntries_ABS);
 
+	scintillator->SetMaterialPropertiesTable(mpt);
+
+	//BaF2
+	std::vector<G4int> natoms_BaF2;
+	std::vector<G4String> elements_BaF2;
+	elements_BaF2.push_back("Ba"); natoms_BaF2.push_back(1);
+	elements_BaF2.push_back("F"); natoms_BaF2.push_back(2);
+	
+	G4Material* scintillator_BaF2 = nist->ConstructNewMaterial("scintillator_BaF2", elements_BaF2, natoms_BaF2, 4.89*g/cm3);
+
+		
+	G4MaterialPropertiesTable *mpt_BaF2 = new G4MaterialPropertiesTable();
+ 	mpt_BaF2->AddConstProperty("SCINTILLATIONYIELD", 11.9/keV); //fast: 1.9, slow: 10
+	mpt_BaF2->AddConstProperty("YIELDRATIO", 0.1597);// fast/total yield
+	mpt_BaF2->AddConstProperty("FASTTIMECONSTANT",0.87*ns);	
+	mpt_BaF2->AddConstProperty("SLOWTIMECONSTANT",630.0*ns);	
+	mpt_BaF2->AddConstProperty("RESOLUTIONSCALE", 1);	
+	
+	const G4int num_BaF2 = 25;
+        G4double ene_BaF2[num_BaF2]   =  {2.76*eV, 2.83*eV, 2.92*eV, 3.01*eV, 3.10*eV,
+					  3.20*eV, 3.31*eV, 3.42*eV, 3.54*eV, 3.67*eV,
+					  3.81*eV, 3.97*eV, 4.13*eV, 4.31*eV, 4.51*eV, 
+					  4.72*eV, 4.96*eV, 5.22*eV, 5.51*eV, 5.83*eV, 
+					  6.20*eV, 6.61*eV, 7.08*eV, 7.63*eV, 8.27*eV};
+                       
+	//fast and slow component, from: https://www.crystals.saint-gobain.com/sites/imdf.crystals.com/files/documents/barium-fluoride-data-sheet_69762.pdf 
+        G4double fast_BaF2[num_BaF2]  =  {0.0, 0.0, 0.0, 0.0, 0.0, 
+					  0.0, 0.0, 0.0, 0.0, 0.0,
+					  0.0, 0.0, 0.0, 0.01, 0.02,
+				          0.04, 0.06, 0.10, 0.24, 0.17,
+					  0.10, 0.12, 0.02, 0.00, 0.00};
+        G4double slow_BaF2[num_BaF2]  =  {0.04, 0.05, 0.08, 0.10, 0.15,
+					  0.20, 0.24, 0.30, 0.42, 0.55, 
+					  0.80, 0.92, 0.90, 0.70, 0.55, 
+		 			  0.30, 0.14, 0.14, 0.24, 0.17,
+					  0.10, 0.12, 0.02, 0.00, 0.00};
+	//refractive index, from: https://refractiveindex.info/?shelf=main&book=BaF2&page=Li
+        G4double rLyso_BaF2[num_BaF2] =  {1.48, 1.48, 1.48, 1.48, 1.48,
+					  1.49, 1.49, 1.49, 1.49, 1.49,
+					  1.50, 1.50, 1.50, 1.50, 1.51,
+					  1.51, 1.52, 1.53, 1.53, 1.54, 
+					  1.56, 1.57, 1.60, 1.63, 1.68};
+        //absorption, from: https://www.crystran.co.uk/optical-materials/barium-fluoride-baf2
+	G4double abs_BaF2[num_BaF2]   =  {500.0*mm, 500.0*mm, 500.0*mm, 500.0*mm, 500.0*mm,
+					  500.0*mm, 500.0*mm, 500.0*mm, 500.0*mm, 500.0*mm,
+					  500.0*mm, 500.0*mm, 500.0*mm, 500.0*mm, 500.0*mm,
+					  500.0*mm, 500.0*mm, 500.0*mm, 500.0*mm, 500.0*mm,
+					  47.6*mm, 35.7*mm, 29.4*mm, 26.9*mm, 14.3*eV};
+		
+	mpt_BaF2->AddProperty("FASTCOMPONENT", ene_BaF2, fast_BaF2, num_BaF2);
+	mpt_BaF2->AddProperty("SLOWCOMPONENT", ene_BaF2, slow_BaF2, num_BaF2);
+	mpt_BaF2->AddProperty("RINDEX", ene_BaF2, rLyso_BaF2 , num_BaF2);
+	mpt_BaF2->AddProperty("ABSLENGTH", ene_BaF2, abs_BaF2 , num_BaF2);
+	
+	scintillator_BaF2->SetMaterialPropertiesTable(mpt_BaF2);
+	
 	//air
     	G4Material* Air  =  nist->FindOrBuildMaterial("G4_AIR");
 	G4double rAir[num] =  {1.00, 1.00, 1.00, 1.00,
@@ -256,7 +311,7 @@ G4VPhysicalVolume *LYSOSiPMDetectorConstruction::DefineVolumes() {
 
     // Get materials
     G4Material* air_mat = G4Material::GetMaterial("G4_AIR");
-    G4Material* cryst_mat = G4Material::GetMaterial("scintillator");
+    G4Material* cryst_mat = G4Material::GetMaterial("scintillator_BaF2");
     G4Material* gel_mat = G4Material::GetMaterial("Silicone");
     G4Material* resin_mat = G4Material::GetMaterial("Resin");
     G4Material* foil_mat = G4Material::GetMaterial("teflon");
